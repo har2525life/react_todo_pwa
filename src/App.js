b06406ce37f454bd58555868b9dc968d8cd50344
hikaru <har2525life@outlook.com>
2022-07-05 18:39:43 +0900

    screen component add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider';
+import Login from './screen/Login';
+import MemoScreen from './screen/MemoScreen';
+import Register from './screen/Register';
 
 
-function App() {
+const App = () => {
   return (
     <AuthProvider>
-      <Header />
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path={`/register/`} element={<Register />} />
+            <Route path={`/login/`} element={<Login />} />
+            <Route path={`/`} element={<MemoScreen />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     </AuthProvider>
   );
 }
---
+++ src/screen/MemoScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MemoScreen = () => {
+    return (
+        <div>MemoScreen</div>
+    )
+}
+
+export default MemoScreen
